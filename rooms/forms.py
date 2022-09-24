6541ce20b85ad09de5f35e2a97b72c5314048219
jeongsangtae <wjdtkdxo55@naver.com>
2022-09-24 23:53:26 +0900

    23.8 Creating Room part One

--- rooms/forms.py
+++ rooms/forms.py
@@ -1,3 +1,4 @@
+from dataclasses import field
 from django import forms
 from django_countries.fields import CountryField
 from . import models
@@ -39,3 +40,31 @@ class CreatePhotoForm(forms.ModelForm):
         room = models.Room.objects.get(pk=pk)
         photo.room = room
         photo.save()
+
+
+class CreateRoomForm(forms.ModelForm):
+    class Meta:
+        model = models.Room
+        fields = (
+            "name",
+            "description",
+            "country",
+            "city",
+            "price",
+            "address",
+            "guests",
+            "beds",
+            "bedrooms",
+            "baths",
+            "check_in",
+            "check_out",
+            "instant_book",
+            "room_type",
+            "amenities",
+            "facilities",
+            "house_rules",
+        )
+
+    def save(self, *args, **kwargs):
+        room = super().save(commit=False)
+        return room
