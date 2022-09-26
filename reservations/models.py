78d2d3056a2aab58e8bef00faaa5d960b0b2193d
jeongsangtae <wjdtkdxo55@naver.com>
2022-09-26 22:56:20 +0900

    24.5 BookedDays part One

--- reservations/models.py
+++ reservations/models.py
@@ -1,7 +1,16 @@
-from time import time
 from django.db import models
 from django.utils import timezone
 from core import models as core_models
+
+
+class BookedDay(core_models.TimeStampedModel):
+
+    day = models.DateField()
+    reservation = models.ForeignKey("Reservation", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Booked Day"
+        verbose_name_plural = "Booked Days"
 
 
 class Reservation(core_models.TimeStampedModel):
@@ -45,3 +54,14 @@ class Reservation(core_models.TimeStampedModel):
         return now > self.check_out
 
     is_finished.boolean = True
+
+    def save(self, *args, **kwargs):
+        if True:
+            start = self.check_in
+            end = self.check_out
+            difference = end - start
+            existing_booked_day = BookedDay.objects.filter(
+                day__range=(start, end)
+            ).exists()
+
+        return super().save(*args, **kwargs)
