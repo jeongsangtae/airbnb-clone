d24784b0d288dd32a0177f80a588fa39f06f438a
jeongsangtae <wjdtkdxo55@naver.com>
2022-09-27 23:25:52 +0900

    #24.14 Reviewing Rooms part One

--- reservations/views.py
+++ reservations/views.py
@@ -1,10 +1,10 @@
 import datetime
-from urllib import request
 from django.http import Http404
 from django.views.generic import View
 from django.contrib import messages
 from django.shortcuts import render, redirect, reverse
 from rooms import models as room_models
+from reviews import forms as review_forms
 from . import models
 
 
@@ -40,8 +40,11 @@ class ReservationDetailView(View):
             and reservation.room.host != self.request.user
         ):
             raise Http404()
+        form = review_forms.CreateReviewForm()
         return render(
-            self.request, "reservations/detail.html", {"reservation": reservation}
+            self.request,
+            "reservations/detail.html",
+            {"reservation": reservation, "form": form},
         )
 
 
