c701aa3330a6299a22aab50f5ac0431c3a183ac0
jeongsangtae <wjdtkdxo55@naver.com>
2022-10-04 17:08:08 +0900

    Reset

--- config/settings.py
+++ config/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = "97vs+41s5a94vb#y$zun7idq549*ui!ysm_1kyfih!7&tyz+ct"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    ".elasticbeanstalk.com",
-]
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -89,26 +87,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if DEBUG is False:
-
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
-else:
-
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "HOST": os.environ.get("RDS_HOST"),
-            "NAME": os.environ.get("RDS_NAME"),
-            "USER": os.environ.get("RDS_USER"),
-            "PASSWORD": os.environ.get("RDS_PASSWORD"),
-            "POST": "5432",
-        }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
